339116b684a5745159c9b91077bbd87f70755dda
Noah Nicolas <53911681+Witop-s@users.noreply.github.com>
2023-09-28 21:25:57 -0400

    Infinity sliding speed fixed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,20 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public float RunSpeed = 3f;
+    public float RunSpeedIncrease = 1.3f;
+    public float RunSpeedDescrease = 0.1f;
+    public float QuickJumpSpeedIncrease = 0.2f;
+    public float QuickJumpForceDecrease = 4f;
     public float WalkSpeed = 1f;
     public float JumpForce = 10f;
     public float SlideForceBoost = 1.2f;
+    public float SlideForceDamping = 0.3f;
     public int Health = 100;
     public float AttackWaitTime = 0.5f;
+    public float AttackFallSpeed = 0.2f;
 
     private bool m_UserJump = false;
+    private bool m_UserQuickJump = false;
     private bool m_IsGrounded = false;
     private bool m_IsSliding = false;
     private bool m_IsAttacking = false;
+    private float StoredRunSpeed;
 
     private Animator m_Animator;
     private Rigidbody2D m_Rigidbody2D;
@@ -30,12 +39,25 @@ public class PlayerController : MonoBehaviour
         m_Rigidbody2D = this.gameObject.GetComponent<Rigidbody2D>();
     }
 
+    private void Start()
+    {
+        StoredRunSpeed = RunSpeed;
+    }
+
     // Update is called once per frame
     private void Update()
     {
         // Jump input
-        if (Input.GetButtonDown("Jump") && m_IsGrounded)
+        if (Input.GetButtonDown("Jump") && m_IsGrounded && !m_IsSliding) {
             m_UserJump = true;
+            m_Animator.SetTrigger("Jump");
+        }
+        else if (Input.GetButtonDown("Jump") && m_IsGrounded && m_IsSliding)
+        {
+            m_UserQuickJump = true;
+            m_Animator.SetTrigger("QuickJump");
+        }
+
 
         // Attack input
         if (Input.GetButtonDown("Fire1") && !m_IsSliding && !m_IsAttacking) {
@@ -54,29 +76,56 @@ public class PlayerController : MonoBehaviour
 
     IEnumerator ResetIsAttacking()
     {
-        yield return new WaitForSeconds(AttackWaitTime);
+        //yield return new WaitForSeconds(AttackWaitTime);
+        // for 2 seconds, the player Y movement is slowed down to 20%
+        float t_Time = 0;
+        while (t_Time < AttackWaitTime)
+        {
+            t_Time += Time.deltaTime;
+            float t_Velocity = m_Rigidbody2D.velocity.y * AttackFallSpeed;
+            m_Rigidbody2D.velocity = new Vector2(m_Rigidbody2D.velocity.x, t_Velocity);
+            yield return null;
+        }
         m_IsAttacking = false;
     }
 
     private void FixedUpdate()
     {
+        //Debug.Log("RunSpeed: " + RunSpeed);
+        //Debug.Log("Velocity: " + m_Rigidbody2D.velocity.x);
+
         float t_MoveX = Input.GetAxis("Horizontal");
         float t_SpeedX = Mathf.Abs(m_Rigidbody2D.velocity.x);
         m_Animator.SetFloat("Speed", Mathf.Abs(t_MoveX));
 
         float t_Force = t_MoveX * RunSpeed;
+        if (Mathf.Abs(t_Force) > WalkSpeed)
+            t_Force = WalkSpeed * Mathf.Sign(t_Force);
         //Debug.Log("Force: " + t_Force);
 
-        if (!m_IsSliding && !m_IsAttacking)
+        if (!m_IsSliding && !m_IsAttacking && m_IsGrounded)
             m_Rigidbody2D.velocity = new Vector2(t_MoveX * (RunSpeed), m_Rigidbody2D.velocity.y);
             //m_Rigidbody2D.AddForce(new Vector2(t_Force, 0), ForceMode2D.Force);
         else if (!m_IsSliding && m_IsAttacking)
             m_Rigidbody2D.velocity = new Vector2(t_MoveX * (WalkSpeed), m_Rigidbody2D.velocity.y);
+        else if (!m_IsSliding && !m_IsAttacking && !m_IsGrounded)
+            m_Rigidbody2D.AddForce(new Vector2(t_Force, 0), ForceMode2D.Force);
         
         // Ground detection
+        bool t_previousGrounded = m_IsGrounded;
         m_IsGrounded = Physics2D.OverlapCircle(m_FeetPos.position, 0.2f, m_GroundLayer) != null ? true : false;
         m_Animator.SetBool("IsGrounded", m_IsGrounded);
 
+        if (m_IsGrounded == true && t_previousGrounded == false) {
+            //m_Animator.SetTrigger("Land");
+            RunSpeed = StoredRunSpeed * RunSpeedIncrease;
+            //Debug.Log("Land or takeoff, i have no idea");
+        }
+        if (RunSpeed > StoredRunSpeed && m_IsGrounded) {
+            // Slowely decrease the run speed
+            RunSpeed -= RunSpeedDescrease;
+            //Debug.Log("RunSpeed: " + RunSpeed);
+        }
  
         // Flip sprite
         if (t_MoveX < 0 && transform.localScale.x > 0 || t_MoveX > 0 && transform.localScale.x < 0 && !m_IsSliding)
@@ -90,7 +139,11 @@ public class PlayerController : MonoBehaviour
             m_IsSliding = true;
             m_Animator.SetBool("IsSliding", true);
             // Add force to the player
-            m_Rigidbody2D.AddForce(Vector2.right * SlideForceBoost * transform.localScale.x, ForceMode2D.Impulse);
+            Vector2 t_Velocity = Vector2.right * SlideForceBoost * transform.localScale.x;
+            if (Mathf.Abs(m_Rigidbody2D.velocity.x) > RunSpeed)
+                t_Velocity = Vector2.right * SlideForceBoost * transform.localScale.x * SlideForceDamping;
+            m_Rigidbody2D.AddForce(t_Velocity, ForceMode2D.Impulse);
+
         }
         else if (Input.GetAxis("Shift") <= 0 && m_IsSliding)
         {
@@ -107,10 +160,25 @@ public class PlayerController : MonoBehaviour
         // Jump
         if (m_UserJump)
         {
+            Debug.Log("Jump");
             m_UserJump = false;
             m_IsGrounded = false;
-            m_Animator.SetTrigger("Jump");
             m_Rigidbody2D.AddForce(Vector2.up * JumpForce, ForceMode2D.Impulse);
+        }
+        else if (m_UserQuickJump)
+        {
+            Debug.Log("QuickJump");
+            m_UserQuickJump = false;
+            m_IsGrounded = false;
+            m_Rigidbody2D.AddForce(Vector2.up * (JumpForce-QuickJumpForceDecrease), ForceMode2D.Impulse);
+            // increase current horizontal velocity by 20%
+            float t_Velocity = m_Rigidbody2D.velocity.x * QuickJumpSpeedIncrease;
+
+            // Calculate the force to be applied with damping
+            //float t_force = Vector2.right.x * t_Velocity * Mathf.Clamp01(1 - (Mathf.Abs(m_Rigidbody2D.velocity.x) / 10));
+            //Debug.Log("Mathf " + Mathf.Clamp01(1 - (Mathf.Abs(m_Rigidbody2D.velocity.x) / 1)));
+
+            //m_Rigidbody2D.AddForce(new Vector2(t_force, 0), ForceMode2D.Impulse);
         }
     }
 
